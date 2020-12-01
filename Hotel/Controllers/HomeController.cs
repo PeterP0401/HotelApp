1cb57423e3475c357beb5b477363beeddd3294a6
Pedro Pech Koh <71104480+PeterP0401@users.noreply.github.com>
2020-12-01 14:22:34 -0600

    Commit 5

--- Hotel/Controllers/HomeController.cs
+++ Hotel/Controllers/HomeController.cs
@@ -7,12 +7,68 @@ using System.Web.Mvc;
 namespace Hotel.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.Title = "Home Page";
 
-            return View();
+    {
+
+        public ActionResult Index()
+
+        {
+
+            LoginModel obj = new LoginModel();
+
+            return View(obj);
+
         }
+
+        [HttpPost]
+
+        public ActionResult Index(LoginModel objuserlogin)
+
+        {
+
+            var display = Userloginvalues().Where(m => m.UserName == objuserlogin.UserName && m.UserPassword == objuserlogin.UserPassword).FirstOrDefault();
+
+            if (display != null)
+
+            {
+
+                ViewBag.Status = "Usuario incorrecto o Contraseña incorrecta";
+
+            }
+
+            else
+
+            {
+
+                ViewBag.Status = "Usuario incorrecto o Contraseña incorrecta";
+
+            }
+
+            return View(objuserlogin);
+
+        }
+
+        public List<LoginModel> Userloginvalues()
+
+        {
+
+            List<LoginModel> objModel = new List<LoginModel>();
+
+            objModel.Add(new LoginModel { UserName = "Pedro", UserPassword = "gr123456" });
+
+            objModel.Add(new LoginModel { UserName = "Angel", UserPassword = "12345" });
+
+            objModel.Add(new LoginModel { UserName = "user3", UserPassword = "password3" });
+
+            objModel.Add(new LoginModel { UserName = "user4", UserPassword = "password4" });
+
+            objModel.Add(new LoginModel { UserName = "user5", UserPassword = "password5" });
+
+            return objModel;
+
+        }
+
     }
+
 }
+
---
+++ UniTestUnix/MailValidatorShould.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+
+
+namespace UniTestUnix
+{
+    public  class MailValidatorShould
+    {
+    }
+}
